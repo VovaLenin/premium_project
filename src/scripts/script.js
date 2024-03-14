81b64497b5816a9285e05353e4dc476d231f48fd
VovaLenin <ustinov2708@gmail.com>
2024-03-14 15:04:56 +0300

    all ready

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,4 +3,19 @@ var swiper = new Swiper(".swiper-container", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  //   stopOnLastSlide: false,
+  //   disableOnInteraction: false,
+  // },
+});
+
+document.getElementById("toggleMenu").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("open");
 });
